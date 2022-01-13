33e518a2e3bb995742316e0000cb8f961962ab66
Stephen Houser <stephenhouser@gmail.com>
2022-01-13 17:05:17 -0500

    Update for newer home assistant

--- owserver/README.md
+++ owserver/README.md
@@ -1,4 +1,4 @@
-HASSIO Addon to expose OneWire devices connected via USB-Serial device on `ttyUSB1`.
+HASSIO Addon to expose OneWire devices connected via usb device.
 
 Devices can be read/written via [OneWire Server Protocol](http://owfs.org/index.php?page=owserver-protocol) over TCP/IP.
 
--- owserver/run.sh
+++ owserver/run.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-set -e
+#!/usr/bin/with-contenv bashio
 
-owserver --error_level=2 -c /etc/owfs.conf --foreground
-
-#mkdir -p /var/run/sshd
-#echo "root:password" | chpasswd 2&> /dev/null
-#sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
-#sed -i s/#LogLevel.*/LogLevel\ DEBUG/ /etc/ssh/sshd_config
-#sed -i s/#PasswordAuthentication.*/PasswordAuthentication\ yes/ /etc/ssh/sshd_config
-#sed -i s/#PermitEmptyPasswords.*/PermitEmptyPasswords\ no/ /etc/ssh/sshd_config
-#exec /usr/sbin/sshd -D -e < /dev/null
+echo "Starting OneWire server..."
+owserver -c /etc/owfs.conf --foreground 
+echo "Done."
